74e865bbe0594485da630829549f45b7f1d7cc7d
Ricardo Santos <ricardos.geral@gmail.com>
2021-07-07 16:33:28 +0100

    initial commit

---
+++ data.js
@@ -0,0 +1,277 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada",
+      "name": "Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.25810894984854826,
+        "pitch": -0.21335236882377018,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0980710920826109,
+          "pitch": 0.1096374397972717,
+          "rotation": 7.853981633974483,
+          "target": "1-manuel-rocha---c-gulbenkian-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2354753769340121,
+          "pitch": -0.4339984536094246,
+          "title": "LNEC",
+          "text": "<span style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">O&nbsp;</span><b style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">Laboratório Nacional de Engenharia Civil, I.P.</b><span style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">&nbsp;(</span><b style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">LNEC</b><span style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">)</span><span style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">&nbsp;é uma instituição pública de investigação e desenvolvimento científico e tecnológico de Portugal</span><span style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">. É um dos maiores laboratórios de engenharia civil</span><span style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">&nbsp;do Mundo. Fica situado na&nbsp;</span><span style=\"color: rgb(32, 33, 34); font-family: sans-serif; font-size: 14px; background-color: rgb(255, 255, 255);\">freguesia de Alvalade em Lisboa.</span>"
+        }
+      ]
+    },
+    {
+      "id": "1-manuel-rocha---c-gulbenkian-",
+      "name": "Manuel Rocha < > C. Gulbenkian ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.68591493077108,
+        "pitch": -0.02629635808107622,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1099617656149725,
+          "pitch": 0.15954745428970618,
+          "rotation": 5.497787143782138,
+          "target": "3-ed-manuel-rocha"
+        },
+        {
+          "yaw": 0.26603058930519907,
+          "pitch": 0.29544124192213594,
+          "rotation": 0,
+          "target": "2-ed-caloust-gulbenkian"
+        },
+        {
+          "yaw": 1.5649176517868568,
+          "pitch": 0.26975942218636817,
+          "rotation": 0.7853981633974483,
+          "target": "0-entrada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-ed-caloust-gulbenkian",
+      "name": "Ed Caloust Gulbenkian",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.013402806375070853,
+        "pitch": -0.04564879135086031,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.02674838009228253,
+          "pitch": 0.03802518555455059,
+          "rotation": 0,
+          "target": "4-geotecnia"
+        },
+        {
+          "yaw": -2.928728889136922,
+          "pitch": 0.15810680028092605,
+          "rotation": 5.497787143782138,
+          "target": "1-manuel-rocha---c-gulbenkian-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.0008995717750153176,
+          "pitch": -0.52695353131719,
+          "title": "LNEC - Geotecnia",
+          "text": "Edifício Calouste Gulbenkian"
+        }
+      ]
+    },
+    {
+      "id": "3-ed-manuel-rocha",
+      "name": "Ed. Manuel Rocha",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.8392798636499208,
+        "pitch": 0.04129798019978281,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6331647324374892,
+          "pitch": 0.32555914197498836,
+          "rotation": 0.7853981633974483,
+          "target": "1-manuel-rocha---c-gulbenkian-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-geotecnia",
+      "name": "Geotecnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.1489978125272362,
+        "pitch": -0.05290127273703682,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0599175919386,
+          "pitch": 0.017063537599732115,
+          "rotation": 10.995574287564278,
+          "target": "5-ricardos-messy-office"
+        },
+        {
+          "yaw": -0.062146060331699005,
+          "pitch": 0.4374559462584884,
+          "rotation": 0,
+          "target": "2-ed-caloust-gulbenkian"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-ricardos-messy-office",
+      "name": "Ricardos messy office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.17671111537965523,
+        "pitch": 0.09706936882318473,
+        "fov": 1.1575709514593322
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.065282055834385,
+          "pitch": 0.1410230801734471,
+          "rotation": 5.497787143782138,
+          "target": "4-geotecnia"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Ricardo's Office",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
